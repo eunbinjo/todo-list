bb4cf3b68120cc092ddad41fa16b279a648d6f95
eunbinjo <joeunbin980916@gmail.com>
2024-02-22 22:13:25 +0900

    0222 momentum 클론 코딩

--- js/background.js
+++ js/background.js
@@ -1,9 +1,11 @@
 const images = ["0.png", "1.png", "2.png"];
-
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+console.log(chosenImage);
 
-bgImage.src = `img/${chosenImage}`;
+const main = document.querySelector("main");
+main.style.backgroundImage = `url(img/${chosenImage})`;
 
-document.main.style.backgroundImage = `url(img/${chosenImage})`;
+// const bgImage = document.createElement("img");
+// bgImage.src = `/img/${chosenImage}`;
+// document.body.appendChild(bgImage);
--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,6 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const loginForm = document.querySelector("#login-form form");
+const loginInput = document.querySelector("#login-form form input");
+const greeting = document.querySelector("#greeting h1");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
